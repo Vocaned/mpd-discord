ec529afea1b308f6ab544b383718177207d122c1
Vocaned <10358674+Vocaned@users.noreply.github.com>
2025-07-14 02:36:50 +0300

    Handle missing data, broken pipes, stopped players etc.

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ class IPC(socket.socket):
         header = struct.pack('<II', op, len(s))
         self.send(header + s.encode('utf-8'))
 
-    def close(self) -> None:
+    def ipc_close(self) -> None:
         self.ipc_write(2, {'v': 1, 'client_id': self.CLIENT_ID})
         op, data = self.ipc_read()
         print(op, data)
-        super().close()
+        self.close()
 
-    def ipc_activity(self, activity: dict) -> tuple[int, dict]:
+    def ipc_activity(self, activity: dict | None) -> tuple[int, dict]:
         payload = {
             'cmd': 'SET_ACTIVITY',
             'args': {
@@ -53,45 +53,80 @@ class IPC(socket.socket):
         self.ipc_write(1, payload)
         return self.ipc_read()
 
+def clean_dict(d):
+    if isinstance(d, dict):
+        return {k: clean_dict(v) for k, v in d.items() if v is not None}
+    return d
+
+def run(*cmd: str) -> dict | None:
+    ret = subprocess.run(cmd, encoding='utf-8', capture_output=True).stdout
+    if not ret.strip():
+        return None # cmd returned nothing
+    return json.loads(ret)
+
+def get_socket() -> IPC:
+    while True:
+        try:
+            return IPC(CLIENT_ID)
+        except ConnectionRefusedError:
+            print('Discord socket not running, trying again in 30 seconds')
+            time.sleep(30)
+
 def main():
-    s = IPC(CLIENT_ID)
-    try:
-        s.ipc_connect()
+    while True:
+        s = get_socket()
+        try:
+            s.ipc_connect()
 
-        while True:
-            status = json.loads(subprocess.run(['rmpc', 'status'], encoding='utf-8', capture_output=True).stdout)
-            song = json.loads(subprocess.run(['rmpc', 'song'], encoding='utf-8', capture_output=True).stdout)
+            while True:
+                status = run('rmpc', 'status')
+                song = run('rmpc', 'song')
+                if not status or not song or status.get('state') != 'Play':
+                    s.ipc_activity(None)
+                    time.sleep(5)
+                    continue
 
-            if status['state'] != 'Play':
+                time_start = int(time.time()) - int(status['elapsed']['secs'])
+                time_end = time_start + int(status['duration']['secs'])
+
+                artist = song['metadata'].get('albumartist') or song['metadata'].get('artist')
+                artistid = song['metadata'].get('musicbrainz_albumartistid') or song['metadata'].get('musicbrainz_artistid')
+                track = song['metadata'].get('title')
+                trackid = song['metadata'].get('musicbrainz_trackid')
+                album = song['metadata'].get('album')
+                albumid = song['metadata'].get('musicbrainz_albumid')
+
+                _, data = s.ipc_activity(clean_dict({
+                    'status_display_type': 1,
+                    'type': 2,
+                    'flags': 1,
+                    'state': artist if artist else None,
+                    'state_url': f'https://musicbrainz.org/artist/{artistid}' if artistid else None,
+                    'details': track if track else None,
+                    'details_url': f'https://musicbrainz.org/track/{trackid}' if trackid else None,
+                    'timestamps': {
+                        'start': time_start * 1000,
+                        'end': time_end * 1000
+                    },
+                    'assets': {
+                        'large_image': f'https://coverartarchive.org/release/{albumid}/front' if albumid else None,
+                        'large_text': album if album else None,
+                        'large_url': f'https://musicbrainz.org/release/{albumid}' if albumid else None
+                    },
+                    'sync_id': trackid
+                }))
+
+                if 'code' in data['data']:
+                    print(data)
+
                 time.sleep(5)
-                continue
+        except KeyboardInterrupt:
+            s.ipc_close()
+            sys.exit(0)
+        except BrokenPipeError:
+            # Restart the main loop
+            s.close()
 
-            time_start = int(time.time()) - int(status['elapsed']['secs'])
-            time_end = time_start + int(status['duration']['secs'])
-
-            _, data = s.ipc_activity({
-                'status_display_type': 1,
-                'type': 2,
-                'state': song['metadata']['albumartist'] or song['metadata']['artist'],
-                'state_url': f"https://musicbrainz.org/artist/{song['metadata']['musicbrainz_albumartistid']}",
-                'details': song['metadata']['title'],
-                'details_url': f"https://musicbrainz.org/track/{song['metadata']['musicbrainz_trackid']}",
-                'timestamps': {
-                    'start': time_start * 1000,
-                    'end': time_end * 1000
-                },
-                'assets': {
-                    'large_image': f"https://coverartarchive.org/release/{song['metadata']['musicbrainz_albumid']}/front",
-                    'large_text': song['metadata']['album'],
-                    'large_url': f"https://musicbrainz.org/release/{song['metadata']['musicbrainz_albumid']}"
-                },
-            })
-
-            print(data)
-
-            time.sleep(5)
-    except KeyboardInterrupt:
-        s.close()
 
 
 if __name__ == '__main__':
